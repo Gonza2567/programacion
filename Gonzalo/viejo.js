0d8d57c967466522f8bcbff2bad5b400e359d3f2
Gonza2567 <gonzaloruizrodriguez3@gmail.com>
2025-05-23 09:33:18 -0300

    Add files via upload

---
+++ Gonzalo/viejo.js
@@ -0,0 +1,17 @@
+let nombre = prompt("Cual es tu nombre")
+let clavedepartamento = prompt("Cual es tu departamento")
+let edad = prompt("Cual es tu edad")
+
+console.log("Tu nombre es", nombre)
+
+console.log("Tu clave de departamento es", clavedepartamento)
+
+console.log("Tu edad es", edad)
+
+
+
+
+
+ //document.getElementById("jorge").innerHTML=    nombre;
+ document.getElementById("Departamento").style.color= "blue";
+ //document.getElementById("edad").innerHTML=    edad;
